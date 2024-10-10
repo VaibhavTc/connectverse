e9757bc3f8755d50dff9aaa4a11d264ba3908e41
VaibhavTc <chakolevaibhav17@gmail.com>
2024-10-10 23:27:54 +0530

    changed index.js routes handling

--- api/index.js
+++ api/index.js
@@ -1,3 +1,49 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import userRoutes from "./routes/user.route.js";
+// import authRoutes from "./routes/auth.route.js";
+// import cookieParser from "cookie-parser";
+// import path from "path";
+// const __dirname = path.resolve();
+// dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use("/api/user", userRoutes);
+// app.use("/api/auth", authRoutes);
+
+// app.use(express.static(path.resolve(__dirname, "client", "dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server listening on port 3000");
+// });
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal Server Error";
+//   return res.status(statusCode).json({
+//     success: false,
+//     message,
+//     statusCode,
+//   });
+// });
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -5,6 +51,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+
 const __dirname = path.resolve();
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
@@ -24,10 +71,12 @@ app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.use(express.static(path.resolve(__dirname, "client", "dist")));
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
+// Catch-all route to serve index.html for SPA
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 app.listen(3000, () => {
