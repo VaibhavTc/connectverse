c65c1f8d3ffc519a338ef6e76f3c6401cc01f96e
VaibhavTc <chakolevaibhav17@gmail.com>
2024-10-10 21:14:45 +0530

    changed directory

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.resolve(__dirname, "client", "dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
